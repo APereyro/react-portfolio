d1036403bf7e4266fe3c2b44fe0d47aab6b461cc
Alan Pereyro <alanpereyro0@gmail.com>
2023-05-31 18:12:59 -0400

    finished up portfolio page and contact page and also readme

--- src/components/PortfolioContainer/pages/Contact.js
+++ src/components/PortfolioContainer/pages/Contact.js
@@ -1,6 +1,7 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import "./contact.css";
 import emailjs from "@emailjs/browser";
+
 
 export default function Contact() {
     const refForm = useRef();
@@ -23,68 +24,63 @@ export default function Contact() {
                 },
             );
     };
-    useEffect(() => {
-      const body = document.querySelector('body');
-      body.style.overflow = 'hidden';
-      return () => {
-        body.style.overflow = 'auto';
-      };
-    }, []);
-  return (
-    <div className="container contact-page" style={{ backgroundColor: "#14213dff", color: "#ffffff", padding: "20px",height: "100vh",width:"100vw",overflow: "hidden" }}>
-      <h1 style={{textAlign:"center"}}>About Page</h1>
-      <div className="text-zone">
-        <p>
-          Greetings! I'm Alan, a dedicated and enthusiastic individual
-          who recently completed a comprehensive coding bootcamp. Throughout the
-          program, I honed my skills in JavaScript, React, HTML, and CSS,
-          equipping me with a strong foundation to thrive in the world of web
-          development. I am eager to apply my newly acquired knowledge and
-          further enhance my abilities in practical, real-world scenarios. I
-          approach challenges with tenacity and have a strong work ethic, always
-          striving to push my boundaries and learn something new every day. If
-          you're interested in connecting and discussing potential
-          opportunities, please feel free to reach out to me by filling out the
-          contact form below. I look forward to engaging with like-minded
-          professionals and contributing to the ever-evolving tech landscape.
-        </p>
-        <div className="contact-form">
-          <form ref={refForm} onSubmit={sendEmail}>
-            <ul>
-              <li className="half">
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Name"
-                  required
-                ></input>
-              </li>
-              <li className="half">
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  required
-                ></input>
-              </li>
-              <li>
-                <textarea
-                  placeholder="Message"
-                  name="message"
-                  required
-                ></textarea>
-              </li>
-              <li>
-                <input style={{ backgroundColor: "#14213dff", color: "#ffffff", }}
-                  type="submit"
-                  className="flat-button"
-                  value="Send"
-                ></input>
-              </li>
-            </ul>
-          </form>
+    return (
+      <div
+        className="container contact-page"
+        style={{
+          backgroundColor: "#14213dff",
+          color: "#ffffff",
+          padding: "20px",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <h1 style={{ textAlign: "center" }}>About Page</h1>
+        <div className="text-zone">
+          <p>
+            Greetings! I'm Alan, a dedicated and enthusiastic individual who recently completed a comprehensive coding bootcamp. Throughout the program, I honed my skills in JavaScript, React, HTML, and CSS, equipping me with a strong foundation to thrive in the world of web development. I am eager to apply my newly acquired knowledge and further enhance my abilities in practical, real-world scenarios. I approach challenges with tenacity and have a strong work ethic, always striving to push my boundaries and learn something new every day. If you're interested in connecting and discussing potential opportunities, please feel free to reach out to me by filling out the contact form below. I look forward to engaging with like-minded professionals and contributing to the ever-evolving tech landscape.
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    required
+                  ></input>
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  ></input>
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input
+                    style={{
+                      backgroundColor: "#14213dff",
+                      color: "#ffffff",
+                    }}
+                    type="submit"
+                    className="flat-button"
+                    value="Send"
+                  ></input>
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
--- src/components/PortfolioContainer/pages/Resume.js
+++ src/components/PortfolioContainer/pages/Resume.js
@@ -1,21 +1,45 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact, faCss3Alt, faHtml5, faJs } from "@fortawesome/free-brands-svg-icons";
 
 export default function Resume() {
   return (
-    <div style={{ backgroundColor: "#14213dff", color: "#e5e5e5ff", padding: "20px",height: "100vh" }}>
+    <div style={{ backgroundColor: "#14213dff", color: "#e5e5e5ff", padding: "20px", height: "100vh", overflow: "hidden" }}>
       <h1>Resume Page</h1>
       <p>
-        Hardworking, ethical, and detail-oriented candidate ready to take on
-        challenges at Publix using problem-solving and task prioritization
-        skills to help my team succeed. I am also looking to further enhance my
-        work experience and knowledge for mutual growth and success.
+        Hello! I recently completed a coding bootcamp where I acquired new skills in web development. Here are some of the technologies I learned:
       </p>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <FontAwesomeIcon
+          icon={faReact}
+          size="5x"
+          style={{ margin: "10px", transition: "color 0.3s ease" }}
+          onMouseOver={(e) => e.target.style.color = "#fca311ff"}
+          onMouseOut={(e) => e.target.style.color = ""}
+        />
+        <FontAwesomeIcon
+          icon={faCss3Alt}
+          size="5x"
+          style={{ margin: "10px", transition: "color 0.3s ease" }}
+          onMouseOver={(e) => e.target.style.color = "#fca311ff"}
+          onMouseOut={(e) => e.target.style.color = ""}
+        />
+        <FontAwesomeIcon
+          icon={faHtml5}
+          size="5x"
+          style={{ margin: "10px", transition: "color 0.3s ease" }}
+          onMouseOver={(e) => e.target.style.color = "#fca311ff"}
+          onMouseOut={(e) => e.target.style.color = ""}
+        />
+        <FontAwesomeIcon
+          icon={faJs}
+          size="5x"
+          style={{ margin: "10px", transition: "color 0.3s ease" }}
+          onMouseOver={(e) => e.target.style.color = "#fca311ff"}
+          onMouseOut={(e) => e.target.style.color = ""}
+        />
+      </div>
+      <p>Also, check out my actual Resume by clicking the Google Drive Icon below!</p>
     </div>
   );
 }
-
-
-{/* <FontAwesomeIcon icon={faReact} />
-<FontAwesomeIcon icon={faCSS3Logo} />
-<FontAwesomeIcon icon={faHTML5Logo} />
-<FontAwesomeIcon icon={faJavaScript(JS)} /> */}
